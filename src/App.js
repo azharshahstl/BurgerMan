11334f4c82739a7b3f6d11dc06ee3dff31f88d25
azharshahstl <azharshahstl@yahoo.com>
2021-03-29 19:01:13 -0500

    building FGurgerbuilder class Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import classes from  './App.module.css';
 import React, { Component } from 'react';
-import layout from './components/Layout/Layout'
+import Layout from './components/Layout/Layout'
 
 
 class App extends Component {
   render() {
   return (
     <div className={classes.App}>
-      <p>hello there!</p>
+      <Layout>
+        <p>hello there!</p>
+      </Layout>
     </div>
   )};
 }
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+class BurgerBuilder extends Component {
+
+    render () { 
+        return (
+            <Aux>
+                <div>
+                    BurgerBuilder
+                    Builder controls
+                </div>
+            </Aux>
+        )};
+};
+
+export default BurerBuilder
