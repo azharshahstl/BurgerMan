7fe078808ee911337f59b63a75bca75dfcd0c9d5
azharshahstl <azharshahstl@yahoo.com>
2021-03-30 18:33:41 -0500

    Adding case statements to BurgerIngredients.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-// import classes from  './App.module.css';
+import classes from  './App.module.css';
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
 import Burgerbuilder from './containers/BurgerBuilder/BurgerBuilder'
 
-
-
 class App extends Component {
   render() {
   return (
-    <div>
+    <div className={classes.App}>
       <Layout>
         <Burgerbuilder/>
       </Layout>
---
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -0,0 +1,15 @@
+import BurgerIngredients from './BurgerIngredients.module.css'
+import React from 'react'; 
+import classes from '*.module.css';
+
+const burgerIngredient = (props) => {
+        let ingredient = null;
+        
+            switch (props.type) {
+                case('Bun-Bottom'): 
+                ingredient = <div className={classes.BunBottom}
+            }
+    
+};
+
+export default burgerIngredient
