0585605b628ea3ea5fee5c6e333b1a3d0abdf353
azharshahstl <azharshahstl@yahoo.com>
2021-03-30 18:45:02 -0500

    Finishing case statements for BurgerIngredients.js

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -7,8 +7,33 @@ const burgerIngredient = (props) => {
         
             switch (props.type) {
                 case('Bun-Bottom'): 
-                ingredient = <div className={classes.BunBottom}
+                    ingredient = <div className={classes.BunBottom} ></div>;
+                    break;
+                case('Bun-Top'): 
+                    ingredient = (
+                        <div className={classes.BunTop}>
+                            <div className={classes.Seeds1}></div>
+                            <div className={classes.Seeds2}></div>
+                        </div>
+                    );
+                    break;
+                case('Meat'): 
+                        ingredient = <div className={classes.Meat}></div>
+                    break;
+                case('Lettuce'): 
+                        ingredient = <div className={classes.Lettuce}></div>
+                    break;
+                case('Cheese'): 
+                        ingredient = <div className={classes.Cheese}></div>
+                    break;
+                case('Bacon'): 
+                        ingredient = <div className={classes.Bacon}></div>
+                    break;
+                default: 
+                        ingredient =  null
             }
+
+        return ingredient;
     
 };
 
