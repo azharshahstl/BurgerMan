1727184887c0f1af34b298e11f24b592a12d1f63
azharshahstl <azharshahstl@yahoo.com>
2021-05-17 14:18:39 -0500

    Improving performance by checking for if the Modal updates

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,24 +2,27 @@ import React from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
+class OrderSummary extends React.Component {
 
-    const summary = Object.keys(props.ingredients).map(ingredkey => {
-        return (<li style={{listStyleType:'none'}} key={ingredkey}>{ingredkey}: {props.ingredients[ingredkey]}</li>)
-    })
+    
 
+    render(){
+        const summary = Object.keys(this.props.ingredients).map(ingredkey => {
+            return (<li style={{listStyleType:'none'}} key={ingredkey}>{ingredkey}: {this.props.ingredients[ingredkey]}</li>)
+        })
     return (
         <Aux>
             <h2>Your Completed Order for Review</h2>
             <ul>
                 {summary}
             </ul>
-            <h3 style={{marginTop: '20px'}}>YOUR TOTAL: ${props.total}</h3>
+            <h3 style={{marginTop: '20px'}}>YOUR TOTAL: ${this.props.total}</h3>
             <p style={{marginTop: '20px'}}>Continue with Checkout?</p>
-            <Button buttonType='Success' clicked={props.purchase}>CONTINUE</Button>
-            <Button buttonType='Cancel' clicked={props.cancel}>CANCEL</Button>
+            <Button buttonType='Success' clicked={this.props.purchase}>CONTINUE</Button>
+            <Button buttonType='Cancel' clicked={this.props.cancel}>CANCEL</Button>
         </Aux>
     )
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,16 +3,31 @@ import classes from './Modal.module.css'
 import Aux from '../../../hoc/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
+class Modal extends React.Component {
+
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.showModal !== this.props.showModal
+    }
+    // We are checking to see if the Modal changes and if it does,
+    // since it wraps the OrderSummary Component, that will update.
+    //  And we don't want to update the order summary when people 
+    // are making their burger, only when we display the order
+    // summary modal. 
+
+    render(){
+        return(
         <Aux>
-            <Backdrop showModal={props.showModal} close={props.closeModal}/>
+            <Backdrop showModal={this.props.showModal} close={this.props.closeModal}/>
             <div className={classes.Modal}
-                style={{transform: props.showModal ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: props.showModal ? '1' : '0'}}>
-                {props.children}
+                style={{transform: this.props.showModal ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.showModal ? '1' : '0'}}>
+                {this.props.children}
             </div>
         </Aux>
+        )
+    }
+        
     
-)
+}
 
-export default modal
+export default Modal
