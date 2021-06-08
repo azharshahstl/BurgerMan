d470fe31ed58798098eebdea09c2794f09e038dd
azharshahstl <azharshahstl@yahoo.com>
2021-06-08 17:31:54 -0500

    added error handling

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,13 +9,19 @@ const withErrorHandler = (WrappedComponent, axios) => {
             error: null
         }
         componentDidMount() {
-            axios.interceptors.request.use(req => {
+           this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null})
                 return req
             })
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error})
             })
+        }
+
+        componentWillUnmount(){
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
+
         }
 
         errorConfirmedHandler = () => {
@@ -27,7 +33,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
             return (
                 <Aux>
                     <Modal show={this.state.error}
-                    clicked={this.errorConfirmedHandler}>
+                    closeModal={this.errorConfirmedHandler}>
                         {this.state.error ? this.state.error.message: null}</Modal>
                     <WrappedComponent { ... this.props} />
     
